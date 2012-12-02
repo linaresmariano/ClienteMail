7ef2282385f10e23409d7ed950e243459da26d53
infradosis <marianolinares92@gmail.com>
2012-12-02 03:09:03 -0300

    Tests de lista de contactos y refactoring de la clase

--- src/contacto/Lista.java
+++ src/contacto/Lista.java
@@ -16,15 +16,7 @@ public class Lista extends Contacto {
 	
 	@Override
 	public boolean contains(Contacto c) {
-		// Recorre la lista de contactos buscando a "c"
-		for(Contacto cont : this.getContactos()) {
-			// Si el contacto actual con tiene a "c" termina
-			if(cont.contains(c)) {
-				return true;
-			}
-		}
-		// Si no lo encuentra devuelve false
-		return false;
+		return this.contactos.contains(c);
 	}
 	
 	// Agrega un contacto a la lista de contactos
