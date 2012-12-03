aa6aff908e26b2b739a095af1caad8f814c63713
jgdimeglio <jgdimeglio@gmail.com>
2012-12-02 22:02:32 -0200

    Update tests/calendario/estrategiasRecordatorias/PorSmsYMailTest.java
    elimine server mock

--- tests/calendario/estrategiasRecordatorias/PorSmsYMailTest.java
+++ tests/calendario/estrategiasRecordatorias/PorSmsYMailTest.java
@@ -15,24 +15,24 @@ public class PorSmsYMailTest {
 		//creando el contexto
 		UsuarioCliente usuario=mock(UsuarioCliente.class);
 		when(usuario.getUsuario()).thenReturn("ejemplo");
-		Server servidor=mock(Server.class);
+		
 		String mensaje="mensaje";
 		Sms sms=mock(Sms.class);
 		PorSmsYMail porMailSms=new PorSmsYMail(usuario,mensaje);
 		porMailSms.setSms(sms);
 		
-		//configurando el servidor y el usuario
-		when(servidor.getDominio()).thenReturn("gmail.com");
-		when(usuario.getServidor()).thenReturn(servidor);
+		//configurandoel usuario
+	
+	
 		when(usuario.getNroCelular()).thenReturn(1525354657);
 		
-	    //realiza la operacion
+	       //realiza la operacion
 		porMailSms.enviarRecordatorio();
 
 		//controlando si se envio correctamente por mail
 		verify(usuario).enviarMail(any(Mail.class));
 		verify(usuario).getUsuario();
-		verify(usuario).getServidor();
+	
 		
 		
 		//contrlando si se envio correctamente pos sms
